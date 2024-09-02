231fb5f185c13268a21e36835924d657abc9501e
Eduardo-Montanhani <montanhani81@gmail.com>
2024-09-02 19:31:01 -0300

    Fazendo as Rotas da Api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,2 +1,27 @@
 <?php
 
+use App\Http\Controllers\Api\v1\CapituloController;
+use App\Http\Controllers\Api\v1\EstadoController;
+use App\Http\Controllers\Api\v1\ObraController;
+use App\Http\Controllers\Api\v1\PaginaController;
+use App\Http\Controllers\Api\v1\PerfilController;
+use App\Http\Controllers\Api\v1\TipoController;
+use App\Http\Controllers\Api\v1\UsuarioController;
+use App\Http\Controllers\ComentarioController;
+use App\Http\Controllers\LeituraController;
+
+use Illuminate\Support\Facades\Route;
+
+ Route::group(['prefix' => 'v1'], function() {
+    Route::apiResource('usuario', UsuarioController::class);
+    Route::apiResource('tipo',TipoController::class);
+    Route::apiResource('capitulo', CapituloController::class);
+    Route::apiResource('leitura', LeituraController::class);
+    Route::apiResource('pagina', PaginaController::class);
+    Route::apiResource('perfil', PerfilController::class);
+    Route::apiResource('estado', EstadoController::class);
+    Route::apiResource('comentario', ComentarioController::class);
+    Route::apiResource('obra', ObraController::class);
+
+    Route::post('usuario/login',[ UsuarioController::class, 'login']);
+ });
