c539d43f76cf4a4323788e0ccefec5097b685b74
Eduardo-Montanhani <montanhani81@gmail.com>
2024-09-20 14:54:33 -0300

    melhorando a parte visual do Home no front

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,4 +1,6 @@
-"use client";
+"use client"
+import styles from './style.module.css';
+
 async function getObras() {
   const res = await fetch("http://127.0.0.1:8000/api/v1/obras", {
     cache: "no-store",
@@ -15,17 +17,25 @@ export default async function Home() {
   const obras = await getObras();
 
   return (
-    <div>
-      <h1>Bem-vindo à página Home</h1>
-      <ul>
-        {obras.map((obra: any) => (
-          <li key={obra.id}>
-            <strong>Título:</strong> {obra.titulo}, <strong>Autor:</strong>{" "}
-            {obra.usuario ? obra.usuario.nome : "Autor não encontrado"},
-            <strong>Likes:</strong> {obra.likes}
-          </li>
-        ))}
-      </ul>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>Bem-vindo à página Home</h1>
+        <ul className={styles.list}>
+          {obras.map((obra: any) => (
+            <li key={obra.id} className={styles.card}>
+              <h2 className={styles.cardTitle}>{obra.titulo}</h2>
+              <p className={styles.cardText}>
+                <strong>Autor:</strong>{" "}
+                {obra.usuario ? obra.usuario.nome : "Autor não encontrado"}
+              </p>
+              <p className={styles.cardText}>
+                <strong>Likes:</strong> {obra.likes}
+              </p>
+              <button className={styles.button}>Ver Detalhes</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
