95b529ea76b6f608a9a6f5dd7ce4867d5104f73d
Thales0820 <thalesaugustosilvalima@gmail.com>
2024-12-02 11:34:39 -0300

    resgatar senha pronto

--- backend/resources/views/auth/forgot-password.blade.php
+++ backend/resources/views/auth/forgot-password.blade.php
@@ -1,5 +1,7 @@
 <link rel="shortcut icon" href="{{ asset('image/logoDark.png') }}" type="image/png">
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<link href="https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet"></link>
+
 <div class="row">
     <!-- @php dump(session()->all()) @endphp -->
     @if(session()->has('status'))
@@ -13,9 +15,10 @@
         @error('email')
         <div class="text text-danger">{{$message}}</div>
         @enderror
-        <input type="text" name="email" placeholder="Seu email">
+        <div class="input">
+            <input type="text" name="email" placeholder="Seu email">
+        </div>
 
         <button type="submit">Enviar Link</button>
     </form>
-
 </div>
